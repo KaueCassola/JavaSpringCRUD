291dd6741ab8e5659226bad3b7040fca2d02209d
KaueCassola <kaue.cassola@hotmail.com>
2023-02-23 18:59:20 -0300

    Update contatoRest.java

--- src/main/java/com/kauecrud/crudjava/contatoRest.java
+++ src/main/java/com/kauecrud/crudjava/contatoRest.java
@@ -3,12 +3,14 @@ package com.kauecrud.crudjava;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kauecrud.crudjava.database.reposCrud;
@@ -16,6 +18,8 @@ import com.kauecrud.crudjava.entidade.contato;
 
 @RestController
 @RequestMapping("/contato")
+@CrossOrigin( origins = {"http:localhost:80", "http://localhost"}, 
+methods = {RequestMethod.GET, RequestMethod.POST})
 public class contatoRest {
 	@Autowired
 	private reposCrud repositorio;
